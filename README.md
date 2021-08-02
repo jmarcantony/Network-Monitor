9a9a344338bb9b925aca89c0f928c19af39c4963
ninjahacker123 <josephmarc2610@gmail.com>
2021-08-02 20:25:40 +0400

    Added Fucntionality To Notify User When Device Connects or Leaves Network

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Network Monitor in Python 👨‍💻
 Programmes like these are to be used on networks you have explicit permission to test. I WILL NOT BE RESPONSIBLE FOR MISUSE OF THIS SOFTWARE!
 
 ## NOTE: Make Sure nmap is installed and is accesible in the terminal
-## *Written in python 3.9.1*
+## *Written in python 3.8.5*
 
 # Installation on Windows:
 * `git clone https://github.com/jmarcantony/Network-Monitor.git`
@@ -16,3 +16,54 @@ Programmes like these are to be used on networks you have explicit permission to
 * `git clone https://github.com/jmarcantony/Network-Monitor.git`
 * `cd Network-Monitor`
 * `python3 main.py`
+
+# Docs:
+## --create-config:
+###  Description:
+    Creates or resets data in config file
+###  Usage:
+	python main.py --create-config
+
+## How To Monitor Adresses:
+### Do you want to get notified when a device you want to monitor conncets or disconncets from you're network, you can do so by tweaking the config json file
+#### By default the config file should be named 'network_monitor_config.json'. The base file should like this
+	{
+		"delay": 5,
+		"hosts_to_monitor": {}
+	}
+#### You can add a host to monitor by adding a host object to the "hosts_to_monitor" key.
+#### eg. 1: lets say you want to monitor 192.168.0.1, the config file should look like this
+	{
+		"delay": 5,
+		"hosts_to_monitor": {
+			"192.168.0.1": {
+				"ringtone": "[PATH TO RINGTONE]",
+				"alert_on_leave": true
+			}
+		}
+	}
+
+#### eg. 2: lets say you want to monitor 192.168.0.1 and 192.168.0.2, the config file should look like this
+	{
+		"delay": 5,
+		"hosts_to_monitor": {
+			"192.168.0.1": {
+				"ringtone": "[PATH TO RINGTONE]",
+				"alert_on_leave": true
+			},
+			"192.168.0.2": {
+				"ringtone": "[PATH TO RINGTONE]",
+				"alert_on_leave": false
+			}
+		}
+	}
+
+#### You can keep adding more host objects while following the appropraite json format
+
+#### For the host object you must give 2 required fields, the 'ringtone' and 'alert_on_leave'
+
+### 'ringtone' field:
+#### The ringtone field should be the path of the audio file when a device connects or disconnects from you're network.
+
+### 'alert_on_leave' field:
+#### This field should be a boolean value where you can set it true if you want to get notified when the device disconnects or false if you do not want to get notified when the device disconnects 
--- main.py
+++ main.py
@@ -1,7 +1,69 @@
 import subprocess
 import datetime
+import json
 import time
+import sys
+import os
 import re
+try:
+    from playsound import playsound
+except ModuleNotFoundError:
+    print(f"[-] Requirements not satisfied\n\trun {'pip' if os.name == 'nt' else 'pip3'} install -r requirements.txt")
+    quit()
+
+CONFIG_FILE_PATH = "network_monitor_config.json"
+HOSTS_TO_MONITOR = {}
+DELAY = 5
+
+def create_config(wipe_data=False):
+    if CONFIG_FILE_PATH not in os.listdir() or wipe_data:
+        with open(CONFIG_FILE_PATH, "w") as f:
+            f.write("""
+{
+    "delay": 5,
+    "hosts_to_monitor": {}
+}
+            """)
+        print("[+] Succesfully Created Config File")
+    else:
+        wipe = True if input("[*] Config File Already Exists, Do you want to wipe the data and create a new one (y)es / (n)o: ").lower() == "y" else False
+        if wipe:
+            create_config(wipe_data=True)
+
+def play_ringtone(path):
+    print(f"[*] Playing Ringtone '{path}'")
+    try:
+        playsound(path)
+    except:
+        print(f"[-] Could Not Play Ringtone '{path}'")
+
+def load_config():
+    global HOSTS_TO_MONITOR
+    global DELAY
+    with open(CONFIG_FILE_PATH) as f:
+        data = json.load(f)
+    try:
+        delay = data["delay"]
+        hosts = {}
+        if not data["hosts_to_monitor"]:
+            return True
+        for host in data["hosts_to_monitor"]:
+            if "ringtone" in data["hosts_to_monitor"][host] and "ring_on_leave" in data["hosts_to_monitor"][host]:
+                if host in hosts:
+                    print("[-] Config Files Are Corrupted (Repitition Of Host Adresses)... Using Default Mode")
+                    return False
+                hosts[host] = data["hosts_to_monitor"][host]
+            else:
+                print("[-] Config Files Are Corrupted (Missing Required Keys For Host Adressess)... Using Default Mode")
+                return False
+    except Exception as e:
+        print("[-] Config Files Are Corrupted (Error In Loading Config File)... Using Default Mode")
+        print(f"Error:\n{e}")
+        return False
+    else:
+        HOSTS_TO_MONITOR = hosts
+        DELAY = delay
+    return True
 
 def get_hosts():
     cmd = "nmap -sP 192.168.0.0/24"
@@ -44,26 +106,44 @@ def main():
         for host in curr_hosts:
             if host not in hosts:
                 print(f"[*] {curr_host_info[host]} ({host}) joined the network!")
+                if host in HOSTS_TO_MONITOR:
+                    ringtone = HOSTS_TO_MONITOR[host]["ringtone"]
+                    play_ringtone(ringtone)
                 changed = True
     
         for host in hosts:
             if host not in curr_hosts:
                 print(f"[*] {host_info[host]} ({host}) left the network!")
+                if host in HOSTS_TO_MONITOR:
+                    if HOSTS_TO_MONITOR[host]["ring_on_leave"]:
+                        ringtone = HOSTS_TO_MONITOR[host]
+                        play_ringtone(ringtone)
                 changed = True
     
         online, hosts = curr_online, curr_hosts
         host_info = curr_host_info
         if not changed:
             print("[*] No change in network so far...")
+
         print("[*] Sleeping")
-        time.sleep(5)
+        time.sleep(DELAY)
 
 if __name__ == "__main__":
+    if "--create-config" in sys.argv and len(sys.argv) == 2:
+        create_config()
+        quit()
+
     if not nmap_exists():
         print("[-] This script can only run with nmap installed")
         quit()
+
     try:
         start = time.time()
+        if CONFIG_FILE_PATH in os.listdir():
+            success = load_config()
+            print(f"\n[*] Successfully Loaded Config Data: {success}\n")
+        else:
+            print(f"\n[*] Config File Not Found... Using Default Mode\n")
         main()
     except KeyboardInterrupt:
         end = time.time()
